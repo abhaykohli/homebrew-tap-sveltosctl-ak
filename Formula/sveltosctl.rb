766d45e71a1dc9e401ba5118b3af43d22aca3934
abhaykohli <42304528+abhaykohli@users.noreply.github.com>
2025-12-23 17:39:12 +0530

    Update Sveltosctl formula with new URL and tags

--- Formula/sveltosctl.rb
+++ Formula/sveltosctl.rb
@@ -2,9 +2,9 @@ class Sveltosctl < Formula
   desc "Sveltos command-line interface"
   homepage "https://projectsveltos.github.io/sveltos/latest/getting_started/sveltosctl/sveltosctl/"
   # url "https://github.com/projectsveltos/sveltosctl.git",
-  url "https://github.com/abhaykohli/sveltosctl-homebrew.git"
-      tag:      "v1.3.3-homebrew-test",
-      revision: "aa87951f5e4f38b3b29903041fe69f71b139e86d"
+url "https://github.com/abhaykohli/sveltosctl-homebrew.git",
+    tag:      "v1.3.3-homebrew-test",
+    revision: "aa87951f5e4f38b3b29903041fe69f71b139e86d"
   license "Apache-2.0"
   head "https://github.com/projectsveltos/sveltosctl.git", branch: "main"
 
